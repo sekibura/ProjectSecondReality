14b075a65c1fbea1c9e39de3681eb23bd41d95ec
Onizuka <51335422+sekibura@users.noreply.github.com>
2022-04-19 13:16:38 +0300

    before add qrScanner tests and refactoring

---
+++ SecondReality/Assets/Editor/Tests/TestQRScanner.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using Altom.AltUnityDriver;
+
+public class TestQRScanner 
+{
+    public AltUnityDriver altUnityDriver;
+    //Before any test it connects with the socket
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        altUnityDriver = new AltUnityDriver();
+    }
+
+    //At the end of the test closes the connection with the socket
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        altUnityDriver.Stop();
+    }
+}
